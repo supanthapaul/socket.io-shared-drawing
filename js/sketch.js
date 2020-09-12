35fcfeed04a21136508e1e9fed35369cbc152c10
Supantha Paul <codewithharsh@gmail.com>
2020-09-12 20:29:41 +0530

    Revert "commit"
    This reverts commit 5cee86dad933473c37cf3617917fea1a485076e5.

--- js/sketch.js
+++ js/sketch.js
@@ -1,47 +1,16 @@
-let rows, cols;
-let scale = 20;
-let w = 1200;
-let h = 900;
-let terrain;
-let flying = 0;
-
 function setup() {
 	// put setup code here
-	createCanvas(600, 600, WEBGL);
-	cols = w/scale;
-	rows = h/scale;
-	// initialize empty 2d matrix
-	terrain = new Array(cols).fill(0).map(() => new Array(rows).fill(0));
-	
+	createCanvas(400, 400);
 }
 
 function draw() {
-	// terrain moving
-	flying -= 0.02;
-	// terrain height generation
-	let yoff = flying;
-	for(let y = 0; y < rows; y++) {
-		let xoff = 0;
-		for(let x = 0; x < cols; x++) {
-			terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-			xoff += 0.15;
-		}
-		yoff += 0.15;
-	}
-
-	background(0);
-	stroke(255);
-	noFill();
-
-	rotateX(PI / 3);
-	translate(-w/2, -h/2);
-	for(let y = 0; y < rows-1; y++) {
-		beginShape(TRIANGLE_STRIP);
-		for(let x = 0; x < cols; x++) {
-			vertex(x* scale, y * scale, terrain[x][y]);
-			vertex(x* scale, (y+1) * scale, terrain[x][y+1]);
-			//rect(x*scale, y*scale, scale, scale);
-		}
-		endShape();
-	}
+	// put drawing code here
+	background(100)
+	if(mouseIsPressed) {
+    fill(0)
+  }
+  else {
+    fill(255); 
+  }
+  ellipse(mouseX, mouseY, 50,50)
 }
